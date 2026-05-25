96aba64bcdd200d2df53730641215511ea0412f1
bumboks <bumboks@gmail.com>
2026-05-25 08:21:46 +0000

    clownface

--- sandwich_order_app.py
+++ sandwich_order_app.py
@@ -6,8 +6,8 @@ from collections import defaultdict
 from zoneinfo import ZoneInfo
 st.set_page_config(
     layout="wide",
-    page_title="B.A.S.S.",
-    page_icon="🎸"
+    page_title="AI SLOP BY MADS",
+    page_icon="🤡"
 )
 
 SMØRREBRØD = [
@@ -178,7 +178,7 @@ if st.session_state.reset_quantities:
     st.session_state.reset_quantities = False
 
 # --- App Title ---
-st.title("🥪 Bestilling Af Smørrebørd og Sandwiches")
+st.title("🥪 (B.A.S.S.) Bestilling Af Smørrebørd og Sandwiches")
 st.markdown("Indtast din bestilling herunder. Alle bestillinger er synlige for alle!")
 
 # --- Price List ---
